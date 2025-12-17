ecbde259fe3dfb4ef1b9e451b8d142f39624ee52
Inrrs <105183522+Inrrs@users.noreply.github.com>
2025-12-17 20:46:07 +0800

    添加微信扫码和手机号登录，删除电报通知添加bark通知

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # 启动 Uvicorn web 服务器 (后台运行)
 echo "Starting Uvicorn server..."
-uvicorn main:app --host 0.0.0.0 --port 6679 &
+uvicorn main:app --host 0.0.0.0 --port 6696 &
 
 # 等待几秒钟，以确保 web 服务器已完全启动
 echo "Waiting for server to start..."
